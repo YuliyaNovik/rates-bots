2a380678feb145dac22e3c636c68571ac5feb179
Yuliya Novik <novik.yuliya.1@gmail.com>
2021-12-08 23:04:13 +0300

    refactored formdata module

--- rates-info-bot/src/http/formdata.js
+++ rates-info-bot/src/http/formdata.js
@@ -5,24 +5,24 @@ const generateBoundary = () => {
         boundary += Math.floor(Math.random() * 10).toString(16);
     }
 
-    return boundary
+    return boundary;
 }
 
-const createMultipartDataBuffer = (boundary, photo, name, fileName) => {
+const createMultipartDataBuffer = (boundary, buffer, name, fileName) => {
     return Buffer.concat([
         Buffer.from(
             `--${boundary}\r\n` +
             `Content-Disposition: form-data; name="${name}"; filename="${fileName}"\r\n` +
             `Content-Type: image/jpeg\r\n\r\n`
         ),
-        photo,
+        buffer,
         Buffer.from(`\r\n--${boundary}--\r\n`)
     ]);
 }
 
-const generateMultipartFormData = (photo, name, fileName) => {
+const generateMultipartFormData = (buffer, name, fileName) => {
     const boundary = generateBoundary();
-    const body = createMultipartDataBuffer(boundary, photo, name, fileName);
+    const body = createMultipartDataBuffer(boundary, buffer, name, fileName);
 
     return { boundary, body };
 }
